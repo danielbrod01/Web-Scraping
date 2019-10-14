2bb73276d7e0fc7a3721f2be9d8ba6b9f2f2efa1
Daniel Rodriguez <daniel.b.rodriguez@gmail.com>
2019-10-14 01:32:14 -0400

    Update webscrape.py
    added a grabber for price tag and and clarity on shipping price

--- webscrape.py
+++ webscrape.py
@@ -12,24 +12,48 @@ url_client.close() # Closes
 # html parsing
 page_soup = soup(page_html, "html.parser")
 
-# Grabs each product (graphics card)
+# Grabs "container" for all products (graphics cards)
 containers = page_soup.findAll("div",{"class":"item-container"})
+
+# Set up csv file
 file_name = "graphics_card_products.csv"
-header_columns = "brand_name, product_name, shipping_price"
+header_columns = "id, brand_name, product_name, price, shipping_price"
 with open(file_name, 'w') as f:
+    # Include header columns on first row of file
     f.write(header_columns + "\n")
+    id = 0
+    # For every product
     for container in containers:
+        # Sets an id number
+        id += 1
+        # Gets brand name
         brand_container = container.find("div",{"class":"item-branding"})
         brand_name = brand_container.a.img["title"]
 
-        title_container = container.find("a",{"class":"item-title"})
-        product_name = title_container.text
+        # Gets product name
+        product_container = container.find("a",{"class":"item-title"})
+        product_name = product_container.text
 
+        # Get product price
+        price_container = container.find("li", {"class":"price-current"})
+        price = price_container.strong.text + price_container.sup.text
+
+        # Gets shipping price
         shipping_container = container.find("li",{"class":"price-ship"})
         shipping_price = shipping_container.text.strip()
+        # Grabs the price digits in the string. "$3.99 shipping" --> "3.99"
+        new_str = []
+        for ch in shipping_price:
+            if ch.isdigit() or ch == '.':
+                new_str.append(str(ch))
+        if new_str:
+            shipping_price = ''.join(new_str)
+        else:
+            shipping_price = "0.00"
 
-        print(brand_name)
-        print(product_name)
-        print(shipping_price + "\n")
+        # Write to file the product details for each row
+        f.write(str(id) + "," + brand_name + "," + product_name.replace("," , "|") + "," + price + "," + shipping_price + "\n")
+    # file closes automatically after the loop ends
 
-        f.write(brand_name + "," + product_name.replace("," , "|") + "," + shipping_price + "\n")
+# When file writing finishes
+print(file_name + " file created!")
