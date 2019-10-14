7b71a218dae2b20543dccf5c76692b5a5651d833
Daniel Rodriguez <daniel.b.rodriguez@gmail.com>
2019-10-13 21:09:49 -0400

    Add files via upload

---
+++ webscrape.py
@@ -0,0 +1,34 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+
+# Opening up connecting, grabbing the page
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close() # Closes
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+
+# Grabs each product (graphics card)
+containers = page_soup.findAll("div",{"class":"item-container"})
+file_name = "graphics_card_products.csv"
+header_columns = "brand_name, product_name, shipping_price"
+with open(file_name, 'w') as f:
+    f.write(header_columns + "\n")
+    for container in containers:
+        brand_container = container.find("div",{"class":"item-branding"})
+        brand_name = brand_container.a.img["title"]
+
+        title_container = container.find("a",{"class":"item-title"})
+        product_name = title_container.text
+
+        shipping_container = container.find("li",{"class":"price-ship"})
+        shipping_price = shipping_container.text.strip()
+
+        print(brand_name)
+        print(product_name)
+        print(shipping_price + "\n")
+
+        f.write(brand_name + "," + product_name.replace("," , "|") + "," + shipping_price + "\n")
